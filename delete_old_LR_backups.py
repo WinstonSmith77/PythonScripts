dfa9b75f7d7ee411ea76e667f6906e8f416321a2
Matthias Henning <matze_henning@gmx.de>
2023-09-20 08:51:41 +0200

    weiter

--- delete_old_LR_backups.py
+++ delete_old_LR_backups.py
@@ -5,12 +5,12 @@ pathToExe = "C:/Program Files/Adobe/Adobe Lightroom Classic/lightroom.exe"
 backupFolder = "C:/Users/matze/OneDrive/lightroom backup"
 
 def delete_old_backups():
-    backups = os.listdir(backupFolder)
+    backups = [item for item in os.scandir(backupFolder)]
     print(backups)
 
 def start_Exe():
     subprocess.run(pathToExe)
 
 if __name__ == "__main__":
-      delete_old_backups()
-      #start_Exe()  
+    delete_old_backups()
+    #start_Exe()  
