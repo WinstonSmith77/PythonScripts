b1d03727236662ea933da35ce48193d8357e622b
Matthias Henning <matze_henning@gmx.de>
2023-09-20 17:51:23 +0200

    witer

--- delete_old_LR_backups.py
+++ delete_old_LR_backups.py
@@ -1,16 +1,24 @@
 import subprocess
 import os
+import pathlib
+import send2trash
 
 pathToExe = "C:/Program Files/Adobe/Adobe Lightroom Classic/lightroom.exe"
 backupFolder = "C:/Users/matze/OneDrive/lightroom backup"
+keepNumberOfBackups = 10
 
 def delete_old_backups():
-    backups = [(item.name) for item in os.scandir(backupFolder) if item.is_file]
-    print(backups)
+    backups = [(item.path, os.path.getctime(item.path)) for item in os.scandir(backupFolder) if item.is_file]
+    backups.sort(key = lambda item : item[0])
+
+    toDelete = backups[0:-keepNumberOfBackups]
+    for item in toDelete:
+        print(item)
+        send2trash.send2trash(item[0].replace('/', "\\"))
 
 def start_Exe():
     subprocess.run(pathToExe)
 
 if __name__ == "__main__":
     delete_old_backups()
-    #start_Exe()  
+    start_Exe()  
