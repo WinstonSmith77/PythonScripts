ab40be0b8e20737c0d13ad6da4b7e35dcd6e780b
Matthias Henning <matze_henning@gmx.de>
2023-09-20 08:22:23 +0200

    geht los

---
+++ deleteLRBackups.py
@@ -0,0 +1,9 @@
+import subprocess
+
+pathToExe = "C:/Program Files/Adobe/Adobe Lightroom Classic/lightroom.exe"
+
+def startExe():
+    subprocess.run(pathToExe)
+
+if __name__ == "__main__":
+      startExe()  
