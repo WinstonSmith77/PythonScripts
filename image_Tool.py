6510ad3f79a009a7140646112c9471e125f8373a
Matthias Henning <matze_henning@gmx.de>
2023-11-30 19:20:03 +0100

    image tool

--- image_Tool.py
+++ image_Tool.py
@@ -5,7 +5,23 @@ working_dir = "C:/Users/matze/OneDrive/bilder"
 
 path = pathlib.Path(working_dir)
 
-all_jpgs =list(path.rglob("*.jpg", case_sensitive= False))
+def get_all_files(path, pattern):
+    return  list(path.rglob(pattern, case_sensitive= False))
 
-print(len(all_jpgs))
+all_jpgs = get_all_files(path, "*.jpg")
+all_raws = get_all_files(path, "*.rw2")
+
+print(all_jpgs[0].parts[-1])
+
+print(all_raws[0].parts[-1])
+
+print(len(all_jpgs), len(all_raws))
+
+hash_jpgs = {file.parts[-1]:file for file in all_jpgs}
+hash_raws = {file.parts[-1]:file for file in all_raws}
+
+
+
+
+print(hash_jpgs, hash_raws)
    
