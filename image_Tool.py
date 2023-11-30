8431646ecf2b5dbbb5703bef82ddfa65d29b30fb
Matthias Henning <matze_henning@gmx.de>
2023-11-30 19:32:32 +0100

    image tool

--- image_Tool.py
+++ image_Tool.py
@@ -17,11 +17,30 @@ print(all_raws[0].parts[-1])
 
 print(len(all_jpgs), len(all_raws))
 
-hash_jpgs = {file.parts[-1]:file for file in all_jpgs}
-hash_raws = {file.parts[-1]:file for file in all_raws}
+
+
+def merge(*args):
+    result = {}
+
+    for image_list in args:
+        for file in image_list:
+            key = file.parts[-1]
+            *_, ext = key.split('.')
+            ext = ext.lower()
+            list_for_name = result.setdefault(key, {})
+            list_for_ext = list_for_name.setdefault(ext, [])
+            list_for_ext.append(file)
+            
+
+    return result
+
+a = merge(all_jpgs, all_raws)
+
+b = {key: value for key, value in a.items() if len(value) > 1}
+pprint.pprint(b)
 
 
 
 
-print(hash_jpgs, hash_raws)
+
    
