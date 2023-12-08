313cd6da5a4e72582f72ee1cafb9292db9028f11
Matthias Henning <henning@geomarketing.de>
2023-12-08 09:28:04 +0100

    weiter

---
+++ prime_numbers.py
@@ -0,0 +1,33 @@
+import itertools
+import math
+import time
+
+def get_primes():
+   
+    yield 1
+    yield 2
+    primes_greater_2 =[]
+
+    for to_check in itertools.count(3, 2):
+        is_prime = True
+        max_to_check = math.floor(math.sqrt(to_check))
+       
+        for known_prime in range(3, max_to_check, 2):
+            if to_check % known_prime == 0:
+                is_prime = False
+                break
+
+        if is_prime:
+            primes_greater_2.append(to_check)
+            yield to_check
+       
+
+
+start = time.time()
+
+primes = list(itertools.islice(get_primes(), 100_000))
+    
+
+end = time.time()    
+
+print(end - start)
