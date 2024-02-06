8184d3f49c865dcf2bf965ce74fd3b87715a6c71
Matthias Henning <henning@geomarketing.de>
2024-02-06 15:09:16 +0100

    weiter

--- hackPW.py
+++ hackPW.py
@@ -5,21 +5,22 @@ import itertools
 import time
 
 valid_key_elements = string.digits + string.ascii_letters
-#print(len(valid_key_elements))
-randomGen = random.Random(-45)
+#valid_key_elements = list(string.digits) + list(string.ascii_letters)
+#print((valid_key_elements))
+randomGen = random.Random()
 length = 4
 
 
 def get_pw(valid_key_elements, l, r : random.Random) -> str:
-    return ''.join(r.choices(valid_key_elements, None, k= l))
+    return tuple(r.choices(valid_key_elements, None, k= l))
 
 def hash_it(pw):
     return ubelt.hash_data(pw , hasher='md5')
 
 pw_to_find = get_pw(valid_key_elements, length, randomGen)
-hash_to_find = hash_it(pw_to_find)
+#hash_to_find = hash_it(pw_to_find)
 
-print(pw_to_find, hash_to_find) 
+print(pw_to_find) 
 
 # def get_pw(ke, random : random.Random, l) -> str:
 #     result = ''
@@ -28,13 +29,12 @@ print(pw_to_find, hash_to_find)
 #     return result    
 
 start = time.time()
-for pw in map(lambda c : ''.join(c), itertools.product(*itertools.repeat(valid_key_elements, length))):
-    hash = hash_it(pw)
-
-
-    if hash_to_find == hash:
+for pw in itertools.product(*itertools.repeat(valid_key_elements, length)):
+   
+    #hash = hash_it(pw)
+    if pw == pw_to_find:
         end = time.time()
-        print(f'found {pw_to_find} {hash} {end - start}')
+        print(f'found {pw} {end - start}')
         break
 
 
