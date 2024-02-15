c48a66300c14580e216ae01d5dfb4a6a062287e3
Matthias Henning <henning@geomarketing.de>
2024-02-15 11:06:33 +0100

    weiter

--- monte-carlo.py
+++ monte-carlo.py
@@ -4,17 +4,20 @@ import math
 total = 0
 in_circle = 0 
 
-random = random.Random()
+random = random.Random(86621)
+diff = 1
+diff_to_break = 1/2_000_000
 
-while True:
+while diff > diff_to_break:
     total += 1
     x,y = random.random(), random.random()
     if x * x + y * y < 1:
         in_circle += 1
 
     pi = 4 * in_circle / total    
+    diff = abs(pi - math.pi)
 
     if total % 1_000_000 == 0:
+        print(round(pi, 7), diff, total)       
 
-        print(round(pi, 7), abs(pi - math.pi))       
-
+print(round(pi, 7), diff, total)     
