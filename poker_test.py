e31217996f206e5d05365bfb8d1a082d3d663a42
Matthias Henning <henning@geomarketing.de>
2024-02-28 09:24:35 +0100

    weiter

--- poker_test.py
+++ poker_test.py
@@ -1,13 +1,14 @@
 from poker import get_all_cards
 import unittest
 
-class CardTests(unittest.TestCase):
 
+class CardTests(unittest.TestCase):
+    cards_per_suit = 13
+    suits = 4
+    total_number_of_cards = suits * cards_per_suit
     def test_number_of_cards(self):
-        self.assertEqual(len( get_all_cards()), 52)
+        self.assertEqual(len(get_all_cards()), CardTests.total_number_of_cards)
+
 
 if __name__ == '__main__':
     unittest.main()
-
-
-
