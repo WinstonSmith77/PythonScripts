34c9a6625f35787be4e3851c6301f6c11993c5d8
Matthias Henning <henning@geomarketing.de>
2024-02-28 09:36:45 +0100

    weiter

--- poker_test.py
+++ poker_test.py
@@ -1,5 +1,6 @@
 from poker import get_all_cards, Suit, Rank
 import unittest
+import itertools
 
 
 class CardTests(unittest.TestCase):
@@ -7,8 +8,8 @@ class CardTests(unittest.TestCase):
     cards_per_suits = 4
     total_number_of_cards = cards_per_suits * cards_per_rank
     
-    def test_number_of_cards(self):
-        self.assertEqual(len(get_all_cards()), CardTests.total_number_of_cards)
+    def test_number_of_distinct_cards(self):
+        self.assertEqual(len(sorted(list(itertools.groupby(get_all_cards())))), CardTests.total_number_of_cards)
 
     def test_number_of_suits(self):
         self.assertEqual(len(Suit), CardTests.cards_per_suits)
