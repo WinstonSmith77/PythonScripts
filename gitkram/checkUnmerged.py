d89fb03b7e83dd9a9e1452388ca58044e96812fe
Matthias Henning <henning@geomarketing.de>
2024-03-08 16:42:07 +0100

    weiter

--- gitkram/checkUnmerged.py
+++ gitkram/checkUnmerged.py
@@ -1,32 +1,35 @@
 import git
 import argparse
 
-
-
-
 def get_conflicted_files(repo):
     """
     Returns a list of unmerged files in the repository.
     """
     conflicted_files = []
+    def is_unmerged(diff):
+         a = bool(diff.a_blob)
+         b = bool (diff.b_blob)
+         return a != b  or (a and b and diff.a_blob.hexsha != diff.b_blob.hexsha)
     for diff in repo.index.diff(None):
-       if diff.a_blob and diff.b_blob and diff.a_blob.hexsha != diff.b_blob.hexsha:
+       if is_unmerged(diff):
             conflicted_files.append(diff.a_blob.path)
     return conflicted_files
 
 if __name__ == "__main__":
     # Replace with the path to your Git repository
-    parser = argparse.ArgumentParser()
-    parser.add_argument("path_repo")
-    args = parser.parse_args()
-    path_repo = args.path_repo
+    path_repo = r'C:\Users\henning\source\easymapGit\dev'
+    if not 'path_repo' in locals():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("path_repo")
+        args = parser.parse_args()
+        path_repo = args.path_repo
 
     repo = git.Repo(path_repo)
 
     conflicted_files = get_conflicted_files(repo)
     if conflicted_files:
-        print("Unmerged files:")
+        print("Conflicted files:")
         for filename in conflicted_files:
             print(filename)
     else:
-        print("No unmerged files found.")
+        print("No conflicted files found.")
