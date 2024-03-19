9c8078aa312d31d501775ae4dc4dd015ab7886ff
Matthias Henning <henning@geomarketing.de>
2024-03-19 16:38:20 +0100

    ants

---
+++ ants.py
@@ -0,0 +1,38 @@
+def to_str(counters):
+    result = ''
+
+    for c in counters:
+        result += str(c) + ' '    
+
+    return result.strip()
+
+def bump_counter(ants):
+    ants = list(ants)
+    L = 'L'
+    R = 'R'
+    
+    number_of_ants = len(ants)
+    counters = [0] * number_of_ants
+    any_bump = True
+
+    while any_bump:
+        any_bump = False
+        for i, l in enumerate(ants):
+            if i == number_of_ants - 1:
+                break 
+            j = i +1
+            if l == R and  ants[j] == L:
+                counters[i] += 1
+                counters[j] += 1
+                ants[i] = L
+                ants[j] = R
+                any_bump = True
+ 
+    return to_str(counters)
+
+print(bump_counter('RRL'))
+
+
+
+
+
