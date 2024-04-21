7bf325c60b3b21ca358e0116411d1e4766229149
Matthias Henning <matze_henning@gmx.de>
2024-04-21 18:55:55 +0200

    weiter

--- poker.py
+++ poker.py
@@ -2,7 +2,7 @@ from enum import IntEnum
 from random import choices
 from itertools import groupby
 from dataclasses import dataclass
-from  pprint import pprint
+from pprint import pprint
 
 
 class CardComponentBase(IntEnum):
@@ -14,56 +14,56 @@ class CardComponentBase(IntEnum):
 
 
 class HandType(CardComponentBase):
-    HIGH = 0,
+    HIGH = (0,)
     PAIR = 1
-    THREE_OF_A_KIND = 2,
-    FULL_HOUSE = 3,
+    THREE_OF_A_KIND = (2,)
+    FULL_HOUSE = (3,)
     FOUR_OF_A_KIND = 4
 
 
 class Suit(CardComponentBase):
-    HEARTS = 0,
-    DIAMONDS = 1,
-    CLUBS = 2,
+    HEARTS = (0,)
+    DIAMONDS = (1,)
+    CLUBS = (2,)
     SPADES = 3
 
 
 class Rank(CardComponentBase):
-    TWO = 0,
-    THREE = 1,
-    FOUR = 2,
-    FIVE = 3,
-    SIX = 4,
-    SEVEN = 5,
-    EIGHT = 6,
-    NINE = 7,
-    TEN = 8,
-    JACK = 9,
-    QUEEN = 10,
-    KING = 11,
+    TWO = (0,)
+    THREE = (1,)
+    FOUR = (2,)
+    FIVE = (3,)
+    SIX = (4,)
+    SEVEN = (5,)
+    EIGHT = (6,)
+    NINE = (7,)
+    TEN = (8,)
+    JACK = (9,)
+    QUEEN = (10,)
+    KING = (11,)
     ACE = 12
 
 
 @dataclass(frozen=True, order=True)
 class Card:
     _subst_suits = {
-        Suit.HEARTS: ['♥','h'],
-        Suit.DIAMONDS: ['♦', 'd'],
-        Suit.CLUBS: ['♣', 'c'],
-        Suit.SPADES: ['♠','s']
-        }
-    
+        Suit.HEARTS: ["♥", "h"],
+        Suit.DIAMONDS: ["♦", "d"],
+        Suit.CLUBS: ["♣", "c"],
+        Suit.SPADES: ["♠", "s"],
+    }
+
     _subst_ranks = {
-        Rank.TWO: ['2'],
-        Rank.THREE: ['3'],
-        Rank.FOUR: ['4'],
-        Rank.FIVE: ['5'],
-        Rank.SIX: ['6'],
-        Rank.SEVEN: ['7'],
-        Rank.EIGHT: ['8'],
-        Rank.NINE: ['9'],
-        Rank.TEN: ['10'],
-        }
+        Rank.TWO: ["2"],
+        Rank.THREE: ["3"],
+        Rank.FOUR: ["4"],
+        Rank.FIVE: ["5"],
+        Rank.SIX: ["6"],
+        Rank.SEVEN: ["7"],
+        Rank.EIGHT: ["8"],
+        Rank.NINE: ["9"],
+        Rank.TEN: ["10"],
+    }
     rank: Rank
     suit: Suit
 
@@ -73,14 +73,14 @@ class Card:
         rank = self.rank
         rank = self._subst_ranks.get(rank, [str(rank)[0]])[0]
 
-        return f'({suit}{rank})'
+        return f"({suit}{rank})"
 
     def __repr__(self):
         return str(self)
-    
+
     @staticmethod
     def resubs(enumType, shortText, subs):
-        for enumValue in enumType: 
+        for enumValue in enumType:
             if enumValue.name[0].lower() == shortText:
                 return enumValue
             if enumValue in subs:
@@ -88,8 +88,8 @@ class Card:
                     return enumValue
 
     @classmethod
-    def parse(cls, shortText : str):
-        shortText = shortText.strip('()')
+    def parse(cls, shortText: str):
+        shortText = shortText.strip("()")
 
         suitText = shortText[0]
         rankText = shortText[1:]
@@ -100,17 +100,18 @@ class Card:
         rank = Card.resubs(Rank, rankText, cls._subst_ranks)
 
         return Card(rank, suit)
-                  
 
-SUITS= list(Suit)
+
+SUITS = list(Suit)
 RANKS = list(Rank)
 
 ALL_CARDS = [Card(rank=rank, suit=suit) for rank in RANKS for suit in SUITS]
 
 
 def get_hand(length):
-    result = choices(ALL_CARDS, k = length)
+    result = choices(ALL_CARDS, k=length)
     return result
+
 
 def get_hand_types(hand):
     result = {HandType.HIGH}
@@ -118,41 +119,42 @@ def get_hand_types(hand):
     def get_rank(x):
         return x.rank
 
-    hand_by_rank =  sorted(hand, key= get_rank)
-    groups_cards = [(k, list(g)) for k,g in groupby(hand_by_rank, key= get_rank)]
-    len_groups = sorted([ len(g) for k , g in groups_cards], reverse=True)
+    hand_by_rank = sorted(hand, key=get_rank)
+    groups_cards = [(k, list(g)) for k, g in groupby(hand_by_rank, key=get_rank)]
+    len_groups = sorted([len(g) for k, g in groups_cards], reverse=True)
 
     has_three = 3 in len_groups
-    has_pair =  2 in len_groups
-    has_four =  4 in len_groups
+    has_pair = 2 in len_groups
+    has_four = 4 in len_groups
 
     if has_pair:
         result.add(HandType.PAIR)
     if has_three:
         result.add(HandType.THREE_OF_A_KIND)
-        #result.add(HandType.PAIR)
+        # result.add(HandType.PAIR)
     if has_three and has_pair:
         result.add(HandType.FULL_HOUSE)
     if has_four:
         result.add(HandType.FOUR_OF_A_KIND)
 
-    return result    
+    return result
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
 
     for c in ALL_CARDS:
         pprint(c)
 
-    pprint(Card.parse('h7'))    
+    pprint(Card.parse("h7"))
 
     number = 5
     length = 8
 
-    total = {type : 0 for type in  HandType}
+    total = {type: 0 for type in HandType}
 
     for i in range(number):
 
-        hand = get_hand(length)  
+        hand = get_hand(length)
         hand_types = get_hand_types(hand)
 
         for type in total:
@@ -161,6 +163,6 @@ if __name__ == '__main__':
     for type in total:
         total[type] = (total[type], total[type] / number)
 
-    total = sorted(total.items(), key= lambda x: x[1][1])    
+    total = sorted(total.items(), key=lambda x: x[1][1])
 
     pprint(total)
