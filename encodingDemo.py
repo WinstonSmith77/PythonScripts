b0aa3d91f233488e41b817f8a51a89e60e7cfe51
Matthias Henning <henning@geomarketing.de>
2024-04-25 09:30:28 +0200

    weiter

---
+++ encodingDemo.py
@@ -0,0 +1,18 @@
+text = "Hänger am Škoda. Mit øl!"
+
+def format_byte_array(bytes):
+    if len(bytes) == 1:
+        return hex(bytes[0])
+    return [hex(c) for c in bytes]
+
+output = [
+            (c,
+              format_byte_array(bytes(c, encoding = "cp1252")),
+              format_byte_array(bytes(c, encoding = 'utf-8')),
+              format_byte_array(bytes(c, encoding = "utf-16")[2:]),
+            ) 
+              
+              for c in text]
+
+for item in output:
+    print(item)
