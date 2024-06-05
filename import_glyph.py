e15d92ba4913e4d9043b1fbf00807b3e21cd8a52
Matthias Henning <henning@geomarketing.de>
2024-06-05 15:51:22 +0200

    weiter

--- import_glyph.py
+++ import_glyph.py
@@ -1,6 +1,7 @@
 import json
 import pathlib
 from collections import namedtuple
+import struct
 
 
 read = json.loads(pathlib.Path('glyph.json').read_text(encoding='utf-8'))
@@ -13,4 +14,34 @@ def parse(data):
 
     return namedtuple('Glyph', ['pixels', 'width', 'height'])(pixels, width, height)
 
-print(parse(read))
+def create_grayscale_bmp_file(bitmap, width, height, filename):
+    # Calculate the size of the image data
+    image_size = width * height
+
+    # Create the BMP file header
+    file_header = struct.pack('<2sIHHI', b'BM', 54 + image_size, 0, 0, 54)
+
+    # Create the BMP info header
+    info_header = struct.pack('<IIIHHIIIIII', 40, width, height, 1, 8, 0, image_size, 0, 0, 0, 0)
+
+    # Create the grayscale pixel data
+    pixel_data = bytes(bitmap)
+
+    # Write the BMP file
+    with open(filename, 'wb') as file:
+        file.write(file_header)
+        file.write(info_header)
+        file.write(pixel_data)
+
+    print(filename)    
+
+bitmap = parse(read)
+
+print(bitmap)
+print('a')
+
+
+
+
+# Usage example
+create_grayscale_bmp_file(bitmap.pixels, bitmap.width, bitmap.height, 'grayscale.bmp')
