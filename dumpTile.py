4e16b09b0ca6a9727e759fca1a6ee1969fcb14a0
Matthias Henning <henning@geomarketing.de>
2024-06-20 16:46:44 +0200

    weiter

--- dumpTile.py
+++ dumpTile.py
@@ -6,36 +6,47 @@ from pathlib import Path
 
 import mapbox_vector_tile
 
+
 def dump_to_file_json(path, jsonData):
-   
-    with open(path, mode="w", encoding='utf8') as file:
+    with open(path, mode="w", encoding="utf8") as file:
         json.dump(jsonData, file, indent=4)
 
 
-url_basemap = 'https://sgx.geodatenzentrum.de/gdz_basemapde_vektor/tiles/v1/bm_web_de_3857/13/4297/2667.pbf'
-url_flur = 'https://maps.infas-lt.de/maps/05CA1656-C77F-4F11-AD88-724AEFBE79ED/prerelease/tiles/collections/grp_ags23_2024/tiles/WebMercatorQuad/12/1374/2173?f=application/vnd.mapbox-vector-tile'
+url_basemap = "https://sgx.geodatenzentrum.de/gdz_basemapde_vektor/tiles/v1/bm_web_de_3857/13/4297/2667.pbf"
+# url_flur = 'https://maps.infas-lt.de/maps/05CA1656-C77F-4F11-AD88-724AEFBE79ED/prerelease/tiles/collections/grp_ags23_2024/tiles/WebMercatorQuad/12/1374/2173?f=application/vnd.mapbox-vector-tile'
 
-urls =  [url_basemap, url_flur]       
+urls = [url_basemap]
 
 pathFile = Path(__file__)
 parent = pathFile.parent
 
-for url  in urls:
+for url in urls:
     read_data = urllib.request.urlopen(url).read()
     data = mapbox_vector_tile.decode(read_data)
-    
-    ulr_ = url.replace('?', '')
+
+    pprint(list(data.keys()))
+
+    data = {k: v for k, v in data.items() if "Verkehrslinie" in k}
+    features_str = "features"
+
+    for v in data.values():
+        features = v[features_str]
+        features = [
+            f
+            for f in features
+            if "properties" in f.keys() and "name" in f["properties"].keys() and 'Arsten' in f['properties']['name']
+        ]
+        v[features_str] = features
+        # del v[features_str]
+
+    ulr_ = url.replace("?", "")
     file = Path(ulr_).parts[-4:]
-    file = '_'.join(file)
-    path = Path(parent, f'{file}.json')
-    path_bin = Path(parent, f'{file}.pbf')
+    file = "_".join(file)
+    path = Path(parent, f"{file}.json")
+    path_bin = Path(parent, f"{file}.pbf")
 
     print(path, path_bin)
 
     Path(path_bin).write_bytes(read_data)
-    
+
     dump_to_file_json(path, data)
-
-
-
-
