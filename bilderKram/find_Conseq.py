e6479bed3a11a56be059e78662945a2923eed4b5
Matthias Henning <matze_henning@gmx.de>
2024-07-01 18:25:53 +0200

    weiter

--- bilderKram/find_Conseq.py
+++ bilderKram/find_Conseq.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+from pprint import pprint
+
 from double_finder.cache import CacheGroup
 from double_finder.find_double_files import get_all_files
 
@@ -6,9 +8,11 @@ from double_finder.find_double_files import get_all_files
 
 working_dir = Path(r"C:\Users\matze\OneDrive\bilder")
 minLength = 1    
+XMP = '.xmp'
 
 
 DIR = 'conseq_dir'
 with CacheGroup(DIR) as caches:
     fs = caches[DIR].lookup(str(working_dir), callIfMissing = lambda: get_all_files(working_dir, '*.*', minLength))
-    print(fs)
+    fs = [path[0] for path in fs if Path(path[0]).suffix.lower() == XMP]
+    pprint(fs)
