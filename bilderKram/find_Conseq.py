7f8c4c9a3f85f4971921895756e1ad96319e6e03
Matthias Henning <matze_henning@gmx.de>
2024-07-01 18:16:48 +0200

    weiter

--- bilderKram/find_Conseq.py
+++ bilderKram/find_Conseq.py
@@ -1,12 +1,14 @@
 from pathlib import Path
 from double_finder.cache import CacheGroup
+from double_finder.find_double_files import get_all_files
 
 
-DIR = 'dir'
-caches = CacheGroup(DIR)
 
 working_dir = Path(r"C:\Users\matze\OneDrive\bilder")
+minLength = 1    
 
 
-
-fs = caches[DIR].lookup(str(working_dir), callIfMissing = lambda: get_all_files(working_dir, '*.*', minLength))
+DIR = 'conseq_dir'
+with CacheGroup(DIR) as caches:
+    fs = caches[DIR].lookup(str(working_dir), callIfMissing = lambda: get_all_files(working_dir, '*.*', minLength))
+    print(fs)
