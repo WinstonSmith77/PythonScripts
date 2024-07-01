bbd8f50770c3572cd9c07f290f8cc2a3b362e784
Matthias Henning <matze_henning@gmx.de>
2024-07-01 18:36:27 +0200

    weiter

--- bilderKram/find_Conseq.py
+++ bilderKram/find_Conseq.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from pprint import pprint
+from xmltodict import parse
 
 from double_finder.cache import CacheGroup
 from double_finder.find_double_files import get_all_files
@@ -14,5 +15,11 @@ XMP = '.xmp'
 DIR = 'conseq_dir'
 with CacheGroup(DIR) as caches:
     fs = caches[DIR].lookup(str(working_dir), callIfMissing = lambda: get_all_files(working_dir, '*.*', minLength))
-    fs = [path[0] for path in fs if Path(path[0]).suffix.lower() == XMP]
-    pprint(fs)
+    files = [path[0] for path in fs if Path(path[0]).suffix.lower() == XMP]
+    #pprint(fs)
+
+    
+
+    xmp = parse(Path(files[5689]).read_text())
+
+    pprint(xmp['x:xmpmeta']['rdf:RDF']['rdf:Description']['@exif:DateTimeOriginal'])
