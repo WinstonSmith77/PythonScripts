1ce5468d32a7d37b3ae7de000f3fce930e57abdd
Matthias Henning <matze_henning@gmx.de>
2024-07-08 20:14:05 +0200

    weiter

--- friday_the_13th.py
+++ friday_the_13th.py
@@ -5,13 +5,15 @@ first_day_gregorian = datetime.datetime(1582, 10, 15)
 end = datetime.datetime(2025, 1, 1)
 
 count_weekdays = {}
-for year in range(first_day_gregorian.year, end.year):
-    for month in range(1, 12):
+for year in range(first_day_gregorian.year, end.year + 1):
+    for month in range(1, 13):
         
         date = datetime.datetime(year, month, 13)
 
         if date < first_day_gregorian or date > end:
             continue
+
+        pprint(date)    
 
         weekday = date.weekday()
         count_weekdays[weekday] = count_weekdays.get(weekday, 0) + 1
