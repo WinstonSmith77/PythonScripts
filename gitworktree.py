3af517c484b1d38cdc0a12f98cbbde18da79b63c
Matthias Henning <henning@geomarketing.de>
2024-07-09 17:14:36 +0200

    weiter

--- gitworktree.py
+++ gitworktree.py
@@ -1,17 +1,14 @@
 import pathlib
 from pprint import pprint
 
-path = pathlib.Path(r'C:\Users\henning\source\easymapGit')
+path = pathlib.Path(r"C:\Users\henning\source\easymapGit")
 
-for file in (path.rglob('*.*', case_sensitive=False)):
-    if file.is_file():
-        
-        if not file.suffix:
-            
-            try:
-                content = file.read_text(encoding='utf-8')
-                if 'c:' in content.lower():
-                    pprint((file, content))
-                    
-            except Exception:
-               pass
+for file in path.rglob("*.*", case_sensitive=False):
+    if file.is_file() and file.suffix:
+        try:
+            content = file.read_text(encoding="utf-8")
+            if "c:" in content.lower():
+                pprint((file, content))
+
+        except Exception:
+            pass
