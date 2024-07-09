b47f951448d71433afa6e7248f7b7b0e1e8bef24
Matthias Henning <henning@geomarketing.de>
2024-07-09 10:27:41 +0200

    weiter

--- friday_the_13th.py
+++ friday_the_13th.py
@@ -4,20 +4,36 @@ from pprint import pprint
 first_day_gregorian = datetime.datetime(1582, 10, 15)
 end = datetime.datetime(2025, 1, 1)
 
+WEEKDAY_NAMES_GERMAN = (
+    "Montag",
+    "Dienstag",
+    "Mittwoch",
+    "Donnerstag",
+    "Freitag",
+    "Samstag",
+    "Sonntag",
+)
+
 count_weekdays = {}
 for year in range(first_day_gregorian.year, end.year + 1):
     for month in range(1, 13):
-        
         date = datetime.datetime(year, month, 13)
 
         if date < first_day_gregorian or date > end:
             continue
 
-        pprint(date)    
-
-        weekday = date.weekday()
+        weekday = WEEKDAY_NAMES_GERMAN[date.weekday()]
         count_weekdays[weekday] = count_weekdays.get(weekday, 0) + 1
 
-count_weekdays = sorted(count_weekdays.items(), key=lambda x: x[1], reverse=True)        
+count_weekdays = sorted(count_weekdays.items(), key=lambda x: x[1], reverse=True)
 
-pprint(count_weekdays)
+min_count = min(count_weekdays, key=lambda x: x[1])[1]
+
+count_weekdays = [
+    (name, f"{count} = {min_count} + {count - min_count}")
+    for name, count in count_weekdays
+]
+
+pprint(f'Start : {first_day_gregorian}')
+pprint(f'End : {end}')
+pprint(count_weekdays, underscore_numbers=True)
