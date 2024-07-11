8d9ab1778cfb76394b6825e3cd17a369c1bf349a
Matthias Henning <henning@geomarketing.de>
2024-07-11 09:22:29 +0200

    weiter

--- #bastel.py
+++ #bastel.py
@@ -1,17 +1,4 @@
-import pathlib
-from pprint import pprint
+import cmath
 
-path = pathlib.Path(r'C:\Users\henning\source\easymapGit')
-
-for file in (path.rglob('*.*', case_sensitive=False)):
-    if file.is_file():
-        
-        if not file.suffix:
-            
-            try:
-                content = file.read_text(encoding='utf-8')
-                if 'c:' in content.lower():
-                    pprint((file, content))
-                    
-            except Exception:
-               pass
+c = complex(1, 1)
+print(cmath.exp(c))
---
+++ randomWalk.py
@@ -0,0 +1,31 @@
+from collections import namedtuple
+from random import Random
+
+State = namedtuple("State", ["account","history"], defaults=[0, []])
+rand = Random()
+
+
+def isSuccess():
+    return rand.randint(0, 1) == 1
+
+
+state = State(account= 100)
+
+def newState(state, delta):
+    return State(state.account + delta, state.history + [state.account])
+
+while True:
+   
+    if state.account <= 0:
+        break
+
+    if(len(state.history) > 100):
+        break
+
+    stake = max(state.account * .5, 0)
+    if isSuccess():
+        state = newState(state, stake)
+    else:
+        state = newState(state, -stake)
+
+print(state.history)
