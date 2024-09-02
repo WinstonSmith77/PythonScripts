970d1e5734e782f51f3b36d0e2692529a331e4db
Matthias Henning <henning@geomarketing.de>
2024-09-02 13:11:51 +0200

    weiter

--- #bastel.py
+++ #bastel.py
@@ -2,28 +2,49 @@ import itertools
 import os
 from pathlib import Path
 from pprint import pprint
+from itertools import groupby
+
 
 def get_length(file):
     stat = os.stat(file)
     return stat.st_size
 
 
-def get_all_files(paths : Path | list[Path], pattern, minLength = 5 * 1024):
+def get_all_files(paths: Path | list[Path], pattern, minLength=5 * 1024):
     if not isinstance(paths, (list, tuple)):
         paths = (paths,)
-    results_sources = (Path(path).rglob(pattern, case_sensitive=False) for path in paths)
+    results_sources = (
+        Path(path).rglob(pattern, case_sensitive=False) for path in paths
+    )
     results = (result for result in itertools.chain(*results_sources))
     results = filter(os.path.isfile, results)
-    results = filter(lambda item : item[1] >= minLength, map(lambda x : (str(x), get_length(x)), results))
+    results = filter(
+        lambda item: item[1] >= minLength,
+        map(lambda x: (str(x), get_length(x)), results),
+    )
 
     return results
 
 
-path = (r'C:\Users\matze\OneDrive\bilder')
+path = r"C:\Users\henning\OneDrive\bilder"
 
 
-all_length =  list(length for _, length in get_all_files(path, '*.rw2'))
+round_to = 5 * 2 * 512 * 1024
 
-min  = min(all_length)
-max = max(all_length)
+all_length = list(
+    length // round_to * round_to for _, length in get_all_files(path, "*.rw2")
+)
+# pprint(all_length)
 
+
+all_length = sorted(all_length)
+all_length = groupby(all_length)
+all_length = {key: len(list(group)) for key, group in all_length}
+
+import matplotlib.pyplot as plt
+
+plt.bar([str(key) for key in all_length.keys()], all_length.values())
+plt.xlabel('File Size')
+plt.ylabel('Count')
+plt.title('File Size Distribution')
+plt.show()
