6a06d9e36a768deaf3cb597d3468954e44af9a19
Matthias Henning <henning@geomarketing.de>
2024-09-04 11:40:00 +0200

    weiter

--- checkStyle.py
+++ checkStyle.py
@@ -6,26 +6,26 @@ from itertools import groupby
 from pprint import pprint
 
 
-
 path = r"bm_web_col.json"
 
+LAYERS = "layers"
+TYPE = "type"
+SYMBOL = "symbol"
+LAYOUT = "layout"
+TEXT_FONT = "text-font"
+ID = "id"
+SOURCE_LAYER = "source-layer"
+PAINT = "paint"
+FILL_COLOR = "fill-color"
 
-LAYERS = 'layers'
-TYPE = 'type'
-SYMBOL = 'symbol'
-LAYOUT = 'layout'
-TEXT_FONT = 'text-font'
-ID = 'id'
-SOURCE_LAYER = 'source-layer'
-PAINT = 'paint'
-FILL_COLOR = "fill-color";
+content = json.loads(pathlib.Path(path).read_text(encoding="utf-8"))
 
-content = json.loads(pathlib.Path(path).read_text(encoding='utf-8'))
-
-styles = [style for style in content[LAYERS]]
-paints = [{key:value for key, value in style.items() if key in [PAINT, ID]} for style in content[LAYERS] if PAINT in style] 
+styles = (style for style in content[LAYERS])
+paints = [
+    {key: value for key, value in style.items() if key in [PAINT, ID]}
+    for style in content[LAYERS]
+    if PAINT in style and any(map(lambda x: x == FILL_COLOR, style[PAINT].keys()))
+]
 
 
 pprint((paints))
-
-
