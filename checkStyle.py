23f61bf8cef34d68c7f350ead46bae37c9788105
Matthias Henning <henning@geomarketing.de>
2024-10-08 12:02:12 +0200

    weiter

--- checkStyle.py
+++ checkStyle.py
@@ -1,6 +1,7 @@
 import json
 import pathlib
 from itertools import groupby
+from typing import Any, Tuple
 
 
 from pprint import pprint
@@ -18,14 +19,25 @@ SOURCE_LAYER = "source-layer"
 PAINT = "paint"
 FILL_COLOR = "fill-color"
 
-content = json.loads(pathlib.Path(path).read_text(encoding="utf-8"))
+MINZOOM = "minzoom"
+MAXZOOM = "maxzoom"
+
+def get_rgb(color: str) :
+    if color.startswith('rgb'):
+        return tuple(map(int, color[4:-1].split(',')))
+    else:
+        return color
+
+def is_blue(color: tuple[int, int, int]) :
+    if not isinstance(color, tuple):
+        return False
+    return color[2] > (color[0] + 20)  and color[2] > (color[1] + 20)
+
+content : dict[str, Any] = json.loads(pathlib.Path(path).read_text(encoding="utf-8"))
 
 styles = (style for style in content[LAYERS])
-paints = [
-    {key: value for key, value in style.items() if key in [PAINT, ID]}
-    for style in content[LAYERS]
-    if PAINT in style and any(map(lambda x: x == FILL_COLOR, style[PAINT].keys()))
-]
+styles = ((style, get_rgb(str(style[PAINT][FILL_COLOR]))) for style in styles if PAINT in style and FILL_COLOR in style[PAINT])
+styles = ((style, color) for (style, color) in styles if is_blue(color))
 
 
-pprint((paints))
+pprint(list(styles))
