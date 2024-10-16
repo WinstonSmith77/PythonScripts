4c463bfe920f70ce6593f3e7b19b4abc7f716219
Matthias Henning <henning@geomarketing.de>
2024-10-16 10:42:50 +0200

    weiter

--- maplibere_render.py
+++ maplibere_render.py
@@ -5,7 +5,7 @@ from typing import Any, Tuple
 
 import urllib.request
 
-from pathlib import Path 
+from pathlib import Path
 from pprint import pprint
 import mapbox_vector_tile
 
@@ -13,16 +13,16 @@ parent = Path(__file__).parent
 path = Path(parent, "##tiles_render.json")
 pathOutput = Path(parent, "##output.txt")
 
+
 def dump_to_file_json(path, jsonData):
     with open(path, mode="w", encoding="utf-8") as file:
         json.dump(jsonData, file, indent=4)
 
+
 def read_tile():
-
-
     if path.exists():
         data = json.loads(path.read_text(encoding="utf-8"))
-    
+
     else:
         url = "https://sgx.geodatenzentrum.de/gdz_basemapde_vektor/tiles/v1/bm_web_de_3857/13/4297/2667.pbf"
         read_data = urllib.request.urlopen(url).read()
@@ -30,6 +30,7 @@ def read_tile():
         dump_to_file_json(path, data)
 
     return data
+
 
 LAYERS = "layers"
 FILTER = "filter"
@@ -48,53 +49,60 @@ LINE_OPACITY = "line-opacity"
 MINZOOM = "minzoom"
 MAXZOOM = "maxzoom"
 
+
 def get_styles():
     path = r"bm_web_col.json"
 
-    def get_rgb(color: str) :
-        if color.startswith('rgb'):
-            return tuple(map(int, color[4:-1].split(',')))
+    def get_rgb(color: str):
+        if color.startswith("rgb"):
+            return tuple(map(int, color[4:-1].split(",")))
         else:
             return color
 
-    def is_very_blue(color: tuple[int, int, int]) :
+    def is_very_blue(color: tuple[int, int, int]):
         if not isinstance(color, tuple):
             return False
-        return color[2] > (color[0] + 20)  and color[2] > (color[1] + 20)
+        return color[2] > (color[0] + 20) and color[2] > (color[1] + 20)
 
-    content : dict[str, Any] = json.loads(pathlib.Path(path).read_text(encoding="utf-8"))
+    content: dict[str, Any] = json.loads(pathlib.Path(path).read_text(encoding="utf-8"))
 
-    def  filter_styles_content(style: dict[str, Any], to_filter : list[str]) -> dict[str, Any]:
-        return {key : value for key, value in style.items() if key in to_filter}
-    
+    def filter_styles_content(
+        style: dict[str, Any], to_filter: list[str]
+    ) -> dict[str, Any]:
+        return {key: value for key, value in style.items() if key in to_filter}
 
-    #styles = [style for style in content[LAYERS]]
-    #styles = (style for style in styles if PAINT in style and FILL_COLOR not in style[PAINT]  )
+    # styles = [style for style in content[LAYERS]]
+    # styles = (style for style in styles if PAINT in style and FILL_COLOR not in style[PAINT]  )
     # styles = ((style, get_rgb(str(style[PAINT][FILL_COLOR]))) for style in styles if PAINT in style and FILL_COLOR in style[PAINT])
     # styles = ((style, color) for (style, color) in styles if is_very_blue(color))
     #
-    stylesDisplay = [filter_styles_content(style, [SOURCE_LAYER, ID, TYPE, FILTER]) for style in content[LAYERS]]
+    stylesDisplay = [
+        filter_styles_content(style, [SOURCE_LAYER, ID, TYPE, FILTER])
+        for style in content[LAYERS]
+    ]
 
-    #types = set(style[TYPE] for style in list(styles))
+    # types = set(style[TYPE] for style in list(styles))
 
-    #stylesForType = {type : [style[ID] for style in stylesDisplay if style[TYPE] == type] for type in types}
+    # stylesForType = {type : [style[ID] for style in stylesDisplay if style[TYPE] == type] for type in types}
 
     return stylesDisplay
 
     #
     # pprint(stylesForType)
 
+
 styles = get_styles()
 tile_data = read_tile()
 
-#pprint(styles)
-#print(tile_data)
+# pprint(styles)
+# print(tile_data)
+
 
 def pass_filter(filter: list, properties: dict[str, Any]) -> bool:
     if not filter:
         return True
-    
-    operation, args = filter[0], filter[1:]  
+
+    operation, args = filter[0], filter[1:]
 
     class Operator:
         ALL = "all"
@@ -105,55 +113,77 @@ def pass_filter(filter: list, properties: dict[str, Any]) -> bool:
         NEQ = "!="
         NHAS = "!has"
         HAS = "has"
+        LESSOREQ = "<="
+        GREATER = ">"
+        GREATEROREQ = ">="
 
-    match operation: 
+        @staticmethod
+        def invert_or_not(operation: str, x: bool) -> bool:
+            if operation in [Operator.NEQ, Operator.NHAS, Operator.NIN]:
+                return not (x)
+            return x
+
+    match operation:
         case Operator.EQ | Operator.IN | Operator.NIN:
             assert len(args) >= 1
-            def invert_or_not(x : bool):
-                if operation == Operator.NEQ:
-                    return not(x)
-                return
-            
             name_prop = args[0]
 
             if name_prop not in properties:
-                return invert_or_not(False)
+                return Operator.invert_or_not(operation, False)
 
             value_prop = properties[name_prop]
-            
+
             compare_to_values = args[1:]
-            must_be_in = [compare_to_value for compare_to_value in compare_to_values] 
-          
-            return invert_or_not(value_prop in must_be_in)
+            must_be_in = [compare_to_value for compare_to_value in compare_to_values]
+
+            return Operator.invert_or_not(operation, value_prop in must_be_in)
+        
         case Operator.ALL | Operator.ANY:
             assert len(args) >= 1
             if operation == Operator.ANY:
                 func = any
-            else: 
-                func = all   
+            else:
+                func = all
             return func(pass_filter(cond, properties) for cond in args)
-        
+
         case Operator.NEQ:
             assert len(args) >= 2
             name_prop = args[0]
-            
+
             if name_prop not in properties:
                 return True
 
             value_prop = properties[name_prop]
-            
+
             must_be = args[1]
             return must_be == value_prop
-        case Operator.NHAS:
+       
+        case Operator.NHAS | Operator.HAS:
             assert len(args) == 1
-            return args[0] not in properties
+            return Operator.invert_or_not(operation, args[0] in properties)
+        
+        case Operator.LESSOREQ:
+            assert len(args) == 2
+            name_prop, value = args
+            return properties.get(name_prop, 0) <= value
+        
+        case Operator.GREATER:
+            assert len(args) == 2
+            name_prop, value = args
+            return properties.get(name_prop, 0) > value
+        
+        case Operator.GREATEROREQ:
+            assert len(args) == 2
+            name_prop, value = args
+            return properties.get(name_prop, 0) >= value
+
         case _:
             assert False, f"Unknown filter: {operation}"
 
     return False
 
-with open(pathOutput, mode="w", encoding="utf-8") as file:
 
+with open(pathOutput, mode="w", encoding="utf-8") as file:
     for style in styles:
         id = style[ID]
         source_layer = style[SOURCE_LAYER]
@@ -170,6 +200,4 @@ with open(pathOutput, mode="w", encoding="utf-8") as file:
             for feature in features:
                 if pass_filter(filter, feature["properties"]):
                     properties = feature["properties"]
-                    print(f'{tab * 2}{properties}', file=file)
-            
-        
+                    print(f"{tab * 2}{properties}", file=file)
