f1729fe72aee65c6204e1254ec5da26cae1a2392
Matthias Henning <henning@geomarketing.de>
2024-10-17 12:18:20 +0200

    weiter

--- maplibere_render.py
+++ maplibere_render.py
@@ -30,7 +30,7 @@ def read_tile():
         url = "https://sgx.geodatenzentrum.de/gdz_basemapde_vektor/tiles/v1/bm_web_de_3857/13/4297/2667.pbf"
         read_data = urllib.request.urlopen(url).read()
         data = mapbox_vector_tile.decode(read_data)
-        #data = None
+        # data = None
         dump_to_file_json(path, data)
 
     return data
@@ -76,7 +76,7 @@ def get_styles():
     ) -> dict[str, Any]:
         return {key: value for key, value in style.items() if key in to_filter}
 
-    styles  = [style for style in content[LAYERS]]
+    styles = [style for style in content[LAYERS]]
     # styles = (style for style in styles if PAINT in style and FILL_COLOR not in style[PAINT]  )
     # styles = ((style, get_rgb(str(style[PAINT][FILL_COLOR]))) for style in styles if PAINT in style and FILL_COLOR in style[PAINT])
     # styles = ((style, color) for (style, color) in styles if is_very_blue(color))
@@ -96,13 +96,14 @@ def get_styles():
     # pprint(stylesForType)
 
 
-
 # pprint(styles)
 # print(tile_data)
 
 
-#lines_oi = None
-lines_oi : set | None = set()
+# lines_oi = None
+lines_oi: set | None = set()
+
+
 def passes_filter(filter: list, properties: dict[str, Any]) -> bool:
     if not filter:
         return True
@@ -126,49 +127,71 @@ def passes_filter(filter: list, properties: dict[str, Any]) -> bool:
             if operation in [Operators.NEQ, Operators.NHAS, Operators.NIN]:
                 return not (x)
             return x
-   
+
     operation = filter[0]
 
     def expand_item(item):
-            if isinstance(item, str) and ',' in item:
-                return item.split(',')
-            return [item]
-    
-    def expand_test_to_set(set_to_test):    
-        return [to_test for to_test in set_to_test for to_test in expand_item(to_test)]
-    
+        if isinstance(item, str) and "," in item:
+            return map(lambda x: x.strip(), item.split(","))
+        return [item]
+
+    def expand_items(set_to_test):
+        return set(
+            to_test for to_test in set_to_test for to_test in expand_item(to_test)
+        )
+
     def tuple_to_str_seperated_by_semikolon(t):
         t = map(str, t)
         return f"({';;'.join(t)})"
-   
-    match filter:
-        case [Operators.EQ | Operators.NEQ | Operators.IN | Operators.NIN, name_prop, *set_to_test]:  
-            value = properties.get(name_prop, None)
-                    
-            result =  Operators.invert_or_not(operation, value in set_to_test)
 
-            if lines_oi is not None and any(map(lambda x: isinstance(x, str) and ',' in x, set_to_test)):
-                if value and ',' in value:
-                    expanded_set = expand_test_to_set(set_to_test)
+    match filter:
+        case [
+            Operators.EQ
+            | Operators.NEQ
+            | Operators.IN
+            | Operators.NIN,
+            name_prop,
+            *set_to_test,
+        ]:
+            value = properties.get(name_prop, None)
+
+            result = Operators.invert_or_not(operation, value in set_to_test)
+
+            if lines_oi is not None and any(
+                map(lambda x: isinstance(x, str) and "," in x, set_to_test)
+            ):
+                if value and "," in value:
+                    expanded_set = expand_items(set_to_test)
                     expanded_value = expand_item(value)
-                    result2 =  Operators.invert_or_not(operation, any(map(lambda x : x in expanded_set, expanded_value)))
+                    result2 = Operators.invert_or_not(
+                        operation, any(map(lambda x: x in expanded_set, expanded_value))
+                    )
                     to_add = "!!!!!" if result != result2 else ""
                     line_to_print = f"{to_add}{value} <> {tuple_to_str_seperated_by_semikolon(tuple(set_to_test))}{to_add}"
                     lines_oi.add(line_to_print)
 
             return result
-        
+
         case [Operators.ALL | Operators.ANY, *sub_filters]:
             if operation == Operators.ANY:
                 any_or_all = any
             else:
                 any_or_all = all
-            return any_or_all(passes_filter(sub_filter, properties) for sub_filter in sub_filters)
+            return any_or_all(
+                passes_filter(sub_filter, properties) for sub_filter in sub_filters
+            )
 
         case [Operators.NHAS | Operators.HAS, name_prop]:
             return Operators.invert_or_not(operation, name_prop in properties)
-        
-        case [Operators.LESSOREQ | Operators.LESS | Operators.GREATER | Operators.GREATEROREQ, name_prop, value]:
+
+        case [
+            Operators.LESSOREQ
+            | Operators.LESS
+            | Operators.GREATER
+            | Operators.GREATEROREQ,
+            name_prop,
+            value,
+        ]:
             if name_prop not in properties:
                 return False
 
@@ -188,6 +211,7 @@ def passes_filter(filter: list, properties: dict[str, Any]) -> bool:
 
     return False
 
+
 def benchmark(f):
     @functools.wraps(f)
     def wrapper(*args, **kwargs):
@@ -200,7 +224,9 @@ def benchmark(f):
 
     return wrapper
 
+
 show_skipped = True
+
 
 @benchmark
 def main():
@@ -221,21 +247,20 @@ def main():
             if MAXZOOM in style:
                 max_zoom = style[MAXZOOM]
             else:
-                max_zoom = ""    
+                max_zoom = ""
 
             if min_zoom != "" and max_zoom != "":
-                zoom_text = f" Zoom: {(min_zoom, max_zoom)}"  
+                zoom_text = f" Zoom: {(min_zoom, max_zoom)}"
             else:
-                zoom_text = None   
-            
+                zoom_text = None
+
             filter = style[FILTER] if FILTER in style else []
-            
-          
+
             if LAYOUT in style and TEXT_FIELD in style[LAYOUT]:
-                text_name  = style[LAYOUT][TEXT_FIELD]
+                text_name = style[LAYOUT][TEXT_FIELD]
             else:
-                text_name = None       
-        
+                text_name = None
+
             style_outputs = []
             if source_layer in tile_data:
                 layer_outputs = []
@@ -246,39 +271,42 @@ def main():
                     properties = feature["properties"]
                     passed = passes_filter(filter, properties)
                     if passed or show_skipped:
-                         layer_outputs.append(f"{"NOT" if not passed else ""}{tab * 2}{properties}")
-                         layer_outputs.append(f"{tab * 3}{feature['geometry']}")
-                         if text_name:
-                            if '{' not in text_name:
+                        layer_outputs.append(
+                            f"{"NOT" if not passed else ""}{tab * 2}{properties}"
+                        )
+                        layer_outputs.append(f"{tab * 3}{feature['geometry']}")
+                        if text_name:
+                            if "{" not in text_name:
                                 text = text_name
                             else:
-                                text_name_stripped = text_name.strip('{}')      
+                                text_name_stripped = text_name.strip("{}")
                                 if text_name_stripped in properties:
                                     text = properties[text_name_stripped]
                                 else:
-                                    text = None        
+                                    text = None
                             if text:
-                                layer_outputs.append(f"{tab * 4}Text: '{text_name}' {text}")
-                           
+                                layer_outputs.append(
+                                    f"{tab * 4}Text: '{text_name}' {text}"
+                                )
+
                 if layer_outputs:
                     style_outputs.append(f'{tab}matches SourceLayer "{source_layer}" ')
                     style_outputs += layer_outputs
-           
+
             if style_outputs:
-                style_outputs.insert(0, f'Styles: "{id}" Filter: "{filter}" {zoom_text if zoom_text else ""}')
+                style_outputs.insert(
+                    0,
+                    f'Styles: "{id}" Filter: "{filter}" {zoom_text if zoom_text else ""}',
+                )
 
                 for output in style_outputs:
-                    print(output, file=file)    
+                    print(output, file=file)
 
-                  
-                
 
-main()        
+main()
 
 
 if lines_oi is not None:
-    with open(pathOfI, mode="w", encoding="utf-8") as file:     
+    with open(pathOfI, mode="w", encoding="utf-8") as file:
         for line_to_print in lines_oi:
-            
-            print(line_to_print, file = file)           
-        
+            print(line_to_print, file=file)
