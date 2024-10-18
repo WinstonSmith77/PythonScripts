b6d0f13cc8c93a5838ce845786b4cce2bc040a1d
Matthias Henning <henning@geomarketing.de>
2024-10-18 12:17:29 +0200

    weiter

--- ratespiel.py
+++ ratespiel.py
@@ -7,14 +7,27 @@ MAXNUMBER = 100
 random_number = randint(MINNUMBER, MAXNUMBER)
 
 
-number = int(input(f"Guess number beween {MINNUMBER} and  {MAXNUMBER}: "))
+def enter_number():
+    valid_input = False
+    while not valid_input:
+        try:
+            number = int(input(f"Guess number beween {MINNUMBER} and  {MAXNUMBER}: "))
+            valid_input = True
+        except ValueError:  # if not a number
+            print("Please enter a number!")
 
-while number != random_number:
+    return number        
+
+
+found_solution = False
+while not found_solution:
+    number = enter_number()
+    if number == random_number:
+        found_solution = True
     if number > random_number:
         print("You guessed too high!")
     elif number < random_number:
         print("You guessed too low!")
-    number = int(input(f"Guess number beween {MINNUMBER} and  {MAXNUMBER}: "))
+
 
 print(f"Congratulations! You guessed the number {random_number} correctly!")
-
