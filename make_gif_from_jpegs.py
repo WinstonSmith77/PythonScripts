f6dc8eb5727df1461e17aa700cb13d95bc55878b
Matthias Henning <matze_henning@gmx.de>
2024-11-29 11:14:31 +0100

    weiter

--- make_gif_from_jpegs.py
+++ make_gif_from_jpegs.py
@@ -4,21 +4,25 @@ import pathlib
 
 # File paths
 
-path = pathlib.Path("C:/Users/matze/Desktop/export/Kinder/hannes_spukt_feuer")
+path = pathlib.Path("C:/Users/matze/Desktop/export/kegeln/mW")
 
-print(path.exists())
+
 
 
 jpeg_files = list(path.rglob("*.JPG"))
+first = jpeg_files[0]
+second = jpeg_files[1]
+jpeg_files = jpeg_files[2:] + [first, second, first, second,first, second, first, second, ]
 
-#print(jpeg_files)
+
+
 
 
 
 output_gif = path / "output.gif"
 
 # Create a list of images
-images = [Image.open(jpeg) for jpeg in jpeg_files]
+images = [Image.open(files) for files in jpeg_files]
 
 # Save as GIF
-images[0].save(output_gif, save_all=True, append_images=images[1:], duration=100, loop=0)
+images[0].save(output_gif, save_all=True, append_images=images[1:], duration=150, loop=0)
