2d530e64efead72006d78a351fd8d216200eaf2d
Matthias Henning <matze_henning@gmx.de>
2024-12-22 19:44:04 +0100

    mac lt

--- delete_old_LR_backups.py
+++ delete_old_LR_backups.py
@@ -1,10 +1,13 @@
 import os
 import shutil
 
-backupFolder = "/Volumes/Matze/matze/Library/CloudStorage/OneDrive-Personal/lightroom_backup_mac"
+backupFolders = ["/Volumes/Matze/matze/Library/CloudStorage/OneDrive-Personal/lightroom_backup_mac", '/Volumes/Matze/matze/Library/CloudStorage/OneDrive-Personal/lightroom backup']
 keepNumberOfBackups = 10
 
 def delete_old_backups(path):
+    if not os.path.exists(path):
+        print(f"Path {path} does not exist")
+        return
     backups = [(item.path, os.path.getctime(item.path)) for item in os.scandir(path) if item.is_dir()]
     backups.sort(key = lambda item : item[0])
 
@@ -13,9 +16,7 @@ def delete_old_backups(path):
         print(item)
         shutil.rmtree(str(item[0]))
         
-
-
-
 if __name__ == "__main__":
-    delete_old_backups(backupFolder)
-    #start_Exe()  
+    for backupFolder in backupFolders:
+        delete_old_backups(backupFolder)
+      
