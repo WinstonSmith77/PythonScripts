c385d9553aeac1b0a5c798de3883e0313f3bfadd
Matthias Henning <henning@geomarketing.de>
2025-02-21 09:40:04 +0100

    weiter

---
+++ einmaleins.py
@@ -0,0 +1,27 @@
+import random
+
+faktor = range(10)
+lern_faktoren = [1, 3, 5, 10]
+
+all_aufgaben = list(set((a + 1, b) for a in faktor for b in lern_faktoren))
+random.shuffle(all_aufgaben)
+
+anzahl = 10
+
+all_aufgaben = all_aufgaben[0:anzahl]
+
+for index, aufgabe in enumerate(all_aufgaben):        
+    richtig = False
+    while not richtig:
+        print(f"{index + 1} von {anzahl}") 
+        print(f"{aufgabe[0]} * {aufgabe[1]} = ?")
+        try:
+            ergebnis = int(input())
+        except ValueError:
+            print("Bitte eine gültige Zahl eingeben.")
+            continue
+        if ergebnis == aufgabe[0] * aufgabe[1]:
+            print("Richtig!")
+            richtig = True
+        else:
+            print("Falsch! Nochmal probieren.")
