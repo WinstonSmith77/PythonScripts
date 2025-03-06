015ab4cbf651a8d289dc0132f3a69eca1701cccb
Matthias Henning <henning@geomarketing.de>
2025-03-06 16:10:50 +0100

    weiter

--- findInTimes.py
+++ findInTimes.py
@@ -1,18 +1,20 @@
 from pathlib import Path
 import datetime
 
-def find_files_in_date_range(folder_path, start_date, end_date, contains=None):
+def find_files_in_date_range(folder_path, time_spans, contains=None):
     files_in_range = []
-    start_datetime = datetime.datetime.strptime(start_date, '%Y-%m-%d %H:%M')
-    end_datetime = datetime.datetime.strptime(end_date, '%Y-%m-%d %H:%M')
 
-   
-    for path in Path(folder_path).rglob('*'):
-        if path.is_file():
-            if contains is not None and contains in str(path):
-                file_modification_time = datetime.datetime.fromtimestamp(path.stat().st_mtime)
-                if start_datetime <= file_modification_time <= end_datetime:
-                    files_in_range.append((str(path), str(file_modification_time)))
+    for timespan in time_spans:
+
+        start_datetime = datetime.datetime.strptime(timespan[0], '%Y-%m-%d %H:%M')
+        end_datetime = datetime.datetime.strptime(timespan[1], '%Y-%m-%d %H:%M')
+    
+        for path in Path(folder_path).rglob('*'):
+            if path.is_file():
+                if contains is not None and contains in str(path):
+                    file_modification_time = datetime.datetime.fromtimestamp(path.stat().st_mtime)
+                    if start_datetime <= file_modification_time <= end_datetime:
+                        files_in_range.append((str(path), str(file_modification_time)))
                 
 
  
@@ -21,7 +23,8 @@ def find_files_in_date_range(folder_path, start_date, end_date, contains=None):
 # Example usage
 folder_path = r'C:/Users/henning/OneDrive/bilder/_lightroom/2025/2025-03-02'
 start_date = '2025-03-02 15:00'
-end_date = '2025-03-02 22:59'
-files = find_files_in_date_range(folder_path, start_date, end_date, "RW2")
+end_date = '2025-03-02 18:00'
+time_sapns = [(start_date, end_date)]
+files = find_files_in_date_range(folder_path, time_sapns, "RW2")
 for file in files:
     print(file)
