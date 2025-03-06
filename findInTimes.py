317ed28d04388b170788e9ce0b96d0bff4f50db1
Matthias Henning <henning@geomarketing.de>
2025-03-06 16:14:13 +0100

    weiter

--- findInTimes.py
+++ findInTimes.py
@@ -21,10 +21,14 @@ def find_files_in_date_range(folder_path, time_spans, contains=None):
     return files_in_range
 
 # Example usage
-folder_path = r'C:/Users/henning/OneDrive/bilder/_lightroom/2025/2025-03-02'
+folder_path = r'C:/Users/henning/OneDrive/bilder/_lightroom/2025'
 start_date = '2025-03-02 15:00'
 end_date = '2025-03-02 18:00'
-time_sapns = [(start_date, end_date)]
+start_date2 = '2025-03-01 15:00'
+end_date2 = '2025-03-01 18:00'
+start_date3 = '2025-02-28 15:00'
+end_date3 = '2025-02-28 18:00'
+time_sapns = [(start_date, end_date, start_date2, end_date2, start_date3, end_date3)]
 files = find_files_in_date_range(folder_path, time_sapns, "RW2")
 for file in files:
     print(file)
