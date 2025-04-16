bc25539b69ccb5d70bb65f5e4c25ec8ed720171b
Matthias Henning <henning@geomarketing.de>
2025-04-16 16:30:41 +0200

    weiter

--- pong.py
+++ pong.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 from random import random
+from collections import namedtuple
 
 # Initialisierung
 pygame.init()
@@ -12,7 +13,8 @@ pygame.display.set_caption("Pong")
 WIDTH_RACKET = 70
 HEIGHT_RACKET = 10
 HEIGHT_BALL = 10
-
+PLAYER_SPEED = 4
+OFFSET_Y_IN_RACKET_HEIGHT = 3
 
 # Farben
 WHITE = (255, 255, 255)
@@ -21,15 +23,17 @@ BLUE = (150, 77, 80)
 GREEN = (60, 200, 199)
 # Spielfigur
 
-OFFSET_Y_IN_RACKET_HEIGHT = 3
-
 player = [WIDTH // 2, HEIGHT - OFFSET_Y_IN_RACKET_HEIGHT * HEIGHT_RACKET]
 
-ball = None
-ball_speed = None
-consecutive_hits = 0
 
-PLAYER_SPEED = 4
+class Ball:
+    def __init__(self, position, speed):
+        self.position = position
+        self.speed = speed
+
+ball = None
+
+consecutive_hits = 0
 
 clock = pygame.time.Clock()
 
@@ -48,40 +52,41 @@ while running:
         player[0] += PLAYER_SPEED
 
     if keys[pygame.K_SPACE]:
-        ball = [float(player[0]), float(player[1])]
         random_x = (random() * 6) - 3
-        ball_speed = [float(random_x), float(-8)]
+        ball = Ball([float(player[0]), float(player[1])],  [float(random_x), float(-8)])
+      
+       
         consecutive_hits = 0
 
     # movement ball
-    if ball and ball_speed:
-        ball[0] += ball_speed[0]
-        ball[1] += ball_speed[1]
+    if ball:
+        ball.position[0] += ball.speed[0]
+        ball.position[1] += ball.speed[1]
 
         # top
-        if ball[1] < 0:
-            ball[1] = -ball[1]
-            ball_speed[1] = -ball_speed[1]
+        if ball.position[1] < 0:
+            ball.position[1] = -ball.position[1]
+            ball.speed[1] = -ball.speed[1]
 
         # left
-        if ball[0] < 0:
-            ball[0] = -ball[0]
-            ball_speed[0] = -ball_speed[0]
+        if ball.position[0] < 0:
+            ball.position[0] = -ball.position[0]
+            ball.speed[0] = -ball.speed[0]
 
         # right
-        if ball[0] > WIDTH:
-            ball[0] = 2 * WIDTH - ball[0]
-            ball_speed[0] = -ball_speed[0]
+        if ball.position[0] > WIDTH:
+            ball.position[0] = 2 * WIDTH - ball.position[0]
+            ball.speed[0] = -ball.speed[0]
 
         # racket
-        if ball[1] > HEIGHT - OFFSET_Y_IN_RACKET_HEIGHT * HEIGHT_RACKET:
-            if ball[0] > player[0] - WIDTH_RACKET // 2 and ball[0] < player[0] + WIDTH_RACKET // 2:
-                ball[1] = player[1] - HEIGHT_RACKET // 2
-                ball[1] = -ball[1]
-                ball_speed = [ball_speed[0] * 1.0, ball_speed[1] * 1.0]
+        if ball.position[1] > HEIGHT - OFFSET_Y_IN_RACKET_HEIGHT * HEIGHT_RACKET:
+            if ball.position[0] > player[0] - WIDTH_RACKET // 2 and ball.position[0] < player[0] + WIDTH_RACKET // 2:
+                ball.position[1] = player[1] - HEIGHT_RACKET // 2
+                ball.position[1] = -ball.position[1]
+                ball_speed = [ball.speed[0] * 1.0, ball.speed[1] * 1.0]
                 consecutive_hits += 1
             else:
-                ball = ball_speed = None
+                ball = None
 
     def render_rect(pos_center, width: int, height: int, color):
         rect = (pos_center[0] - width // 2, pos_center[1] - height //
@@ -97,7 +102,7 @@ while running:
     render_rect(player, WIDTH_RACKET, HEIGHT_RACKET, RED)
 
     if ball:
-        render_rect(ball, HEIGHT_BALL, HEIGHT_BALL, BLUE)
+        render_rect(ball.position, HEIGHT_BALL, HEIGHT_BALL, BLUE)
 
     # font = pygame.font.SysFont('Comic Sans MS', 30)
     # text_surface = font.render(
@@ -108,7 +113,6 @@ while running:
     text_surface = font.render(
         str(consecutive_hits), False, (0, 0, 0))
     win.blit(text_surface, (0, 0))
-
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
