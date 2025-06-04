e494600a765fc55cfee5709e6424904700586dc4
Matthias Henning <henning@geomarketing.de>
2025-06-04 13:39:15 +0200

    weiter

--- check_wmts_file.py
+++ check_wmts_file.py
@@ -3,8 +3,10 @@ import xml.etree.ElementTree as ET
 xml_file = 'WMTSList.xml'
 providers = 'wmts_providers'
 url = 'provider_url'
+url_hint = 'usage_hint_url'
 
 all_urls : list[str] = []
+all_hint_providers : list[str] = []
 
 try:
     tree = ET.parse(xml_file)
@@ -17,6 +19,9 @@ try:
             provider_url = entry.attrib.get(url)
             if provider_url:
                 all_urls.append(provider_url)
+            provider_url_hint = entry.attrib.get(url_hint)
+            if provider_url_hint:
+                all_hint_providers.append(provider_url_hint)    
                
     else:
         print("'wmts_providers' not found in the XML.")
@@ -24,3 +29,4 @@ except Exception as e:
     print(f"Error opening XML file: {e}")
 
 print(all_urls)    
+print(all_hint_providers)    
