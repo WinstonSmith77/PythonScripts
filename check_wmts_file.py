f9d012a63aa82c68ec7cee078404123b8b96ebc7
Matthias Henning <henning@geomarketing.de>
2025-06-04 13:36:32 +0200

    weiter

--- check_wmts_file.py
+++ check_wmts_file.py
@@ -1,18 +1,26 @@
 import xml.etree.ElementTree as ET
 
-# Replace 'your_file.xml' with the path to your XML file
 xml_file = 'WMTSList.xml'
+providers = 'wmts_providers'
+url = 'provider_url'
+
+all_urls : list[str] = []
+
 try:
     tree = ET.parse(xml_file)
     root = tree.getroot()
     print(f"Root tag: {root.tag}")
 
-    # Find all entries under 'wmts_providers'
-    wmts_providers = root.find('wmts_providers')
+    wmts_providers = root.find(providers)
     if wmts_providers is not None:
         for entry in wmts_providers:
-            print(ET.tostring(entry, encoding='unicode'))
+            provider_url = entry.attrib.get(url)
+            if provider_url:
+                all_urls.append(provider_url)
+               
     else:
         print("'wmts_providers' not found in the XML.")
 except Exception as e:
     print(f"Error opening XML file: {e}")
+
+print(all_urls)    
