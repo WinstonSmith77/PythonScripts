bfc5c6412a07047da60140184089457d85edc935
Matthias Henning <henning@geomarketing.de>
2025-06-04 13:48:14 +0200

    weiter

--- check_wmts_file.py
+++ check_wmts_file.py
@@ -1,3 +1,5 @@
+import requests
+
 import xml.etree.ElementTree as ET
 
 xml_file = 'WMTSList.xml'
@@ -29,5 +31,19 @@ try:
 except Exception as e:
     print(f"Error opening XML file: {e}")
 
-print(all_urls)    
-print(all_hint_providers)    
+def check_url_exists(url):
+    try:
+        response = requests.head(url, allow_redirects=True, timeout=5)
+        return response.status_code == 200
+    except Exception as e:
+        return False
+
+non_existing_urls = [u for u in all_urls if not check_url_exists(u)]
+print("Non Existing URLs:")
+print(non_existing_urls)
+  
+
+non_existing_hint_urls = [u for u in all_hint_providers if not check_url_exists(u)]
+print("Non Existing Hint URLs:")
+print(non_existing_hint_urls)
+    
