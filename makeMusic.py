a973639de9f1acf6f491032e6fefe355e0adef9c
Matthias Henning <matze_henning@gmx.de>
2025-06-05 06:14:40 +0200

    weiter

--- makeMusic.py
+++ makeMusic.py
@@ -4,7 +4,7 @@ import shutil
 stick  = pathlib.Path ("/Volumes/Matze/matze/Desktop/usb stick")
 
 
-def copy_files_to_usb(source_folder: str, dest_folder: str | pathlib.Path):
+def copy_files_to_usb(source_folder: str | pathlib.Path, dest_folder: str | pathlib.Path):
     """Copy all files from the source folder to the USB stick folder."""
     source = pathlib.Path(source_folder)
     dest = pathlib.Path(dest_folder)
@@ -32,19 +32,22 @@ stick.mkdir(parents=True, exist_ok=True)
 trashes_path = stick / ".trashes"
 pathlib.Path(trashes_path).touch()
 
-copy_files_to_usb('/Volumes/Matze/matze/Library/CloudStorage/OneDrive-Personal/iTunes Music/Music/Arthur Conan Doyle', stick / 'Krimis')
-copy_files_to_usb('/Volumes/Matze/matze/Library/CloudStorage/OneDrive-Personal/iTunes Music/neue musik/In Vino Veritas', stick / 'Krimis' / 'In Vino Veritas')
-copy_files_to_usb('/Volumes/Matze/matze/Library/CloudStorage/OneDrive-Personal/iTunes Music/neue musik/stelter', stick / 'Krimis' / 'Stelter')
-copy_files_to_usb('/Volumes/Matze/matze/Library/CloudStorage/OneDrive-Personal/iTunes Music/neue musik/Agatha Christie', stick / 'Krimis' / 'Agatha Christie')
+itunes_path = pathlib.Path('/Volumes/Matze/matze/Library/CloudStorage/OneDrive-Personal/iTunes Music')
+music_path = itunes_path / 'Music'
 
-copy_files_to_usb('/Volumes/Matze/matze/Library/CloudStorage/OneDrive-Personal/iTunes Music/Music/Dirk Bach', stick / 'Walter Moers')
-copy_files_to_usb('/Volumes/Matze/matze/Library/CloudStorage/OneDrive-Personal/iTunes Music/Music/Walter Moers', stick / 'Walter Moers')
-copy_files_to_usb('/Volumes/Matze/matze/Library/CloudStorage/OneDrive-Personal/iTunes Music/neue musik/Moers_EInhörnchen', stick / 'Walter Moers' / 'Einhörnchen')
+copy_files_to_usb(music_path / 'Arthur Conan Doyle', stick / 'Krimis')
+copy_files_to_usb(itunes_path / 'neue musik/In Vino Veritas', stick / 'Krimis' / 'In Vino Veritas')
+copy_files_to_usb(itunes_path / 'neue musik/stelter', stick / 'Krimis' / 'Stelter')
+copy_files_to_usb(itunes_path / 'neue musik/Agatha Christie', stick / 'Krimis' / 'Agatha Christie')
 
-copy_files_to_usb('/Volumes/Matze/matze/Library/CloudStorage/OneDrive-Personal/iTunes Music/neue musik/Horst Evers', stick / 'Horst Evers')
+copy_files_to_usb(music_path / 'Dirk Bach', stick / 'Walter Moers')
+copy_files_to_usb(music_path / 'Walter Moers', stick / 'Walter Moers')
+copy_files_to_usb(itunes_path / 'neue musik/Moers_EInhörnchen', stick / 'Walter Moers' / 'Einhörnchen')
 
-copy_files_to_usb('/Volumes/Matze/matze/Library/CloudStorage/OneDrive-Personal/iTunes Music/Music/Marc-Uwe Kling', stick / 'Marc-Uwe Kling')
+copy_files_to_usb(itunes_path / 'neue musik/Horst Evers', stick / 'Horst Evers')
+
+copy_files_to_usb(music_path / 'Marc-Uwe Kling', stick / 'Marc-Uwe Kling')
 
 
-copy_files_to_usb('/Volumes/Matze/matze/Library/CloudStorage/OneDrive-Personal/iTunes Music/Music/Die Drei ___', stick / 'Die Drei Fragezeichen')
+copy_files_to_usb(music_path / 'Die Drei ___', stick / 'Die Drei Fragezeichen')
 
