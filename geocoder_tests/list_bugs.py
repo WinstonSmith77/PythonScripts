bffcff7c27b62f82381d6968ad9fb4670d3ba9ec
Matthias Henning <henning@geomarketing.de>
2025-06-17 16:52:06 +0200

    weiter

--- geocoder_tests/list_bugs.py
+++ geocoder_tests/list_bugs.py
@@ -4,14 +4,26 @@ import xml.etree.ElementTree as ET
 
 
 script_path = Path(os.path.abspath(__file__))
-path_xml = script_path.parent / "tests.xml"
-print(f"Current script path: {script_path}")
 
 
-tree = ET.parse(path_xml)
-root = tree.getroot()
-children = list(root)
+fails = ["failed_geocode.xml"]
 
-for child in children:
-    print({child.attrib["name"]})
+for fail in fails:
+
+    path_xml = script_path.parent / fail
+
+
+    print(f"Current script path: {script_path}")
+
+    def read_xml(path):
+        tree = ET.parse(path)
+        root = tree.getroot()
+        return root
+    
+
+    children = list(read_xml(path_xml))
+    
+
+    for child in children:
+        print(child.attrib["name"])
 
