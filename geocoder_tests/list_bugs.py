f6b7e11c02ccd94789ba93615bbea435ec9b8fcb
Matthias Henning <henning@geomarketing.de>
2025-06-17 17:18:29 +0200

    weiter

--- geocoder_tests/list_bugs.py
+++ geocoder_tests/list_bugs.py
@@ -1,29 +1,30 @@
 import os
 from pathlib import Path
+import json
 import xml.etree.ElementTree as ET
 
 
 script_path = Path(os.path.abspath(__file__))
 
 
-fails = ["failed_geocode.xml"]
+fails = ["failed_geocode.xml", "failed_singleline.xml"]
+
+output = script_path.parent / "output.text"
+
+fail_text = []
 
 for fail in fails:
-
     path_xml = script_path.parent / fail
-
-
-    print(f"Current script path: {script_path}")
 
     def read_xml(path):
         tree = ET.parse(path)
         root = tree.getroot()
         return root
-    
 
     children = list(read_xml(path_xml))
-    
 
     for child in children:
-        print(child.attrib["name"])
+        fail_text.append(child.attrib["name"] + "\n" + child.attrib["status"] + "\n\n")
 
+with open(output, "w", encoding="utf-8") as f:
+    f.writelines(fail_text)
