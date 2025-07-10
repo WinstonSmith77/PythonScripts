3d7a0df0d0b73e0e7027bf1151ae5ca77dbdbc88
Matthias Henning <henning@geomarketing.de>
2025-07-10 14:05:07 +0200

    weiter

--- ausgleich.py
+++ ausgleich.py
@@ -1,14 +1,11 @@
-hat_bezahlt = (("Sven", 0), ("Gunnar", 10),  ("Kevin", 70),  ("Danny", 25), ("Matze", 20), ("Torsten", 0))
-
-
+hat_bezahlt = (("Matthias", 168.11), ("Roberto", 59),  ("Jens M.", 65),  ("Jens G.", 0), ("Stefan", 12))
 
 def ausgleich(bezahlt):
     schnitt = sum([x[1] for x in bezahlt]) / len(bezahlt)
-    bezahlt_korrigiert = dict((x[0], x[1] - schnitt) for x in bezahlt)
+    bezahlt_korrigiert = {x[0] : (x[1] - schnitt) for x in bezahlt}
 
     ausgleiche = []
 
-   # pprint(bezahlt_korrigiert)
     while(True):
         bezahlt_korrigiert = {k: v for k, v in bezahlt_korrigiert.items() if abs(v) >=  0.01}
 
@@ -23,11 +20,10 @@ def ausgleich(bezahlt):
         ausgleiche.append((min_bezahlt[0], max_bezahlt[0], betrag_ausgleich))
         bezahlt_korrigiert[max_bezahlt[0]] -= betrag_ausgleich
         bezahlt_korrigiert[min_bezahlt[0]] += betrag_ausgleich
-
    
     return ausgleiche
 
-ausgleiche =(ausgleich(hat_bezahlt))   
+ausgleiche = ausgleich(hat_bezahlt)  
 
 for bezahlt in hat_bezahlt:
     print(bezahlt[0], "hat", round(bezahlt[1], 2), "Euro bezahlt")
