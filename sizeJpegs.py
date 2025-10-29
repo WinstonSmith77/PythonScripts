150cb83abbb9016b3fb6064da37fad4953cd92c8
Matthias Henning <matze_henning@gmx.de>
2025-10-29 06:38:21 +0100

    weiter

--- sizeJpegs.py
+++ sizeJpegs.py
@@ -3,13 +3,16 @@ from PIL import Image
 from PIL.ExifTags import TAGS
 
 def get_creation_date(file_path):
+   # print(f"Getting creation date for: {file_path}")
     try:
         image = Image.open(file_path)
         exif_data = image._getexif()
         if exif_data:
             for tag_id, value in exif_data.items():
-                tag = TAGS.get(tag_id, tag_id)
-                if tag == 'DateTimeOriginal':
+                tag = TAGS.get(tag_id)
+               # print(f"Tag: {tag}, Value: {value}")
+                if tag == 'DateTimeOriginal' or tag == 'DateTime':
+                    #print(f"Found creation date: {value} {tag}")
                     return value
     except Exception:
         pass
@@ -23,10 +26,11 @@ def sum_jpeg_sizes(folder_path):
                 file_path = os.path.join(root, file)
                 total_size += os.path.getsize(file_path)
                 creation_date = get_creation_date(file_path)
-                print(f"{file}: Creation date: {creation_date}")
+                
+               # print(f"{file}: Creation date: {creation_date}")
     return total_size
 
 if __name__ == "__main__":
     folder = "/Volumes/Matze/matze/Library/CloudStorage/OneDrive-Personal/bilder"
     total = sum_jpeg_sizes(folder)
-    print(f"Total size of all JPEG files: {total/1024/1024} mb")
+    print(f"Total size of all JPEG files: {total/1024/1024/1024:.2f} GB")
