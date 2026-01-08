3aaa734ab2bab781a45c74fc2bfcfa32e96e5912
Matthias Henning <matthias.henning@ipsos.com>
2026-01-08 11:33:07 +0100

    weiter

--- stylesToTest/filterAllLoaderAllStyles.py
+++ stylesToTest/filterAllLoaderAllStyles.py
@@ -12,11 +12,11 @@ LINE_WIDTH_KEY = "line-width"
 LINE_DASH_ARRAY = "line-dasharray"
 
 
-
 # Get all JSON style files from the current directory
 style_folder = os.path.dirname(os.path.abspath(__file__))
 style_files = glob.glob(os.path.join(style_folder, "*.json"))
-style_files = list(filter(lambda f:  "bm_web_col_new" in str(os.path.basename(f)), style_files))
+style_files = list(
+    filter(lambda f:  "bm_web_col_new" in str(os.path.basename(f)), style_files))
 
 
 # Print all found style files
@@ -33,21 +33,30 @@ for style_file in style_files:
             print(f"File: {os.path.basename(style_file)}")
             print('=' * 60)
 
-          
             layer_names = map(lambda x: x[LAYER_ID_KEY], content[LAYERS_KEY])
-            layer_names = sorted(layer_names, key=lambda s: len(s), reverse=True)
+            layer_names = sorted(
+                layer_names, key=lambda s: len(s), reverse=True)
 
-            #pprint(layer_names)
+            # pprint(layer_names)
+
+            def dash_fits(dash):
+                if isinstance(dash, dict):
+                    if 'stops' in dash:
+                        return True
+                return False
 
             if LAYERS_KEY in content:
-               for layer in content[LAYERS_KEY]:
-                    
-                    if PAINT_KEY in layer:
-                        if LINE_DASH_ARRAY in layer[PAINT_KEY]:
-                           print(f"{layer[LAYER_ID_KEY]} {layer[PAINT_KEY][LINE_DASH_ARRAY]}")
-                                
+                for layer in content[LAYERS_KEY]:
+                   # if layer[LAYER_ID_KEY] == "Gewaesser_L_Durchlass_Dueker":
+
+                        if PAINT_KEY in layer:
+                            if LINE_DASH_ARRAY in layer[PAINT_KEY]:
+                                dash = layer[PAINT_KEY][LINE_DASH_ARRAY]
+                                if dash_fits(dash):
+                                    print(f"{layer[LAYER_ID_KEY]} {dash}")
+
             else:
-               print("No 'layers' key found in this file")
+                print("No 'layers' key found in this file")
 
         except json.JSONDecodeError as e:
             print(f"\nError parsing {os.path.basename(style_file)}: {e}")
