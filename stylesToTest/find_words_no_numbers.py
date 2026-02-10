c66db6428461bf332cc15b3c511daa8cc7fd322f
Matthias Henning <matthias.henning@ipsos.com>
2026-02-10 16:06:19 +0100

    weiter

--- stylesToTest/find_words_no_numbers.py
+++ stylesToTest/find_words_no_numbers.py
@@ -1,24 +1,37 @@
 from pathlib import Path
 from collections import Counter
 from pprint import pprint
-import itertools 
+import itertools
 
 
+TRANSLATION_TABLE = str.maketrans({
+    '"': None,
+    "'": None,
+    " ": None,
+    ",": " ",
+    ":": " ",
+    "[": " [ ",
+    "]": " ] ",
+    "{": " { ",
+    "}": " } ",
+})
 
-def count_words(text : str):
-    
+
+def count_words(text: str):
+    text = text.translate(TRANSLATION_TABLE)
+
     splits = text.split()
-    return [word for word in splits if not any(ch.isdigit() for ch in word)]
+    return (word for word in splits if not any(ch.isdigit() for ch in word))
 
 parent = Path(__file__).parent
 files = list(parent.rglob('*.json'))
 
 
-words = list(itertools.chain.from_iterable(count_words(file.read_text(encoding='UTF8')) for file in files))
+words = itertools.chain.from_iterable(count_words(file.read_text(encoding='UTF8')) for file in files)
 
 
-counter = {item: count for item, count in Counter(words).items() if count > 1}
+counter = sorted(  {item: count for item, count in Counter(words).items() if count > 250}.items(), key= lambda x : x[1] )
 
 
-pprint(counter)
+print(counter)
 
