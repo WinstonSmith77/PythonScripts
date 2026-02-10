51f7980c15d4a73e381e5f1818c35ed867978130
Matthias Henning <matthias.henning@ipsos.com>
2026-02-10 15:26:07 +0100

    weiter

--- stylesToTest/find_words_no_numbers.py
+++ stylesToTest/find_words_no_numbers.py
@@ -1,13 +1,24 @@
 from pathlib import Path
 from collections import Counter
 from pprint import pprint
+import itertools 
+
+
+
+def count_words(text : str):
+
+    splits = text.split()
+    return [word for word in splits if not any(ch.isdigit() for ch in word)]
 
 parent = Path(__file__).parent
 files = list(parent.rglob('*.json'))
 
-first = files[0]
 
-print(first)
+words = list(itertools.chain.from_iterable(count_words(file.read_text(encoding='UTF8')) for file in files))
 
 
+counter = {item: count for item, count in Counter(words).items() if count > 1}
+
+
+pprint(counter)
 
