36567cdb59da52c3bc4810ae128d6914e286d240
Matthias Henning <matthias.henning@ipsos.com>
2026-02-27 11:22:53 +0100

    weiter

--- cubic.py
+++ cubic.py
@@ -7,5 +7,7 @@ eq2 = a*x2**3 + b*x2**2 + c*x2 + d - y2
 eq3 = 3*a*x1**2 + 2*b*x1 + c - k1
 eq4 = 3*a*x2**2 + 2*b*x2 + c - k2
 sol = sympy.solve([eq1, eq2, eq3, eq4], [a, b, c, d])
+
+
 for k,v in sol.items():
-    print(k,v)
+    print('var ', k,'=',sympy.ccode(v), ";")
